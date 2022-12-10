173576958a500d1d96157652350e69fcd642cf4c
Esdras B Silva <bzr4@icloud.com>
2022-12-10 16:04:37 -0300

    - Alteracao de FutureBuilder para HomePage para facilitar o envio de dados as demais paginas.

--- lib/pages/currencies/currencies_page.dart
+++ lib/pages/currencies/currencies_page.dart
@@ -1,87 +1,49 @@
 import 'package:f290_acf_dindin/model/currency.dart';
-import 'package:f290_acf_dindin/service/currency_service.dart';
 import 'package:flutter/material.dart';
-import 'package:loading_indicator/loading_indicator.dart';
 
 class CurrenciesPage extends StatelessWidget {
-  CurrenciesPage({super.key});
+  const CurrenciesPage({super.key, required this.currencies});
 
-  final service = CurrencyService();
+  final List<Currency> currencies;
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      initialData: [],
-      future: service.getCurrencies(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.none ||
-            snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-              child: SizedBox(
-            height: 200,
-            width: 200,
-            child: LoadingIndicator(
-              colors: [
-                Colors.amberAccent,
-              ],
-              indicatorType: Indicator.lineSpinFadeLoader,
+    return ListView.builder(
+        itemCount: currencies.length,
+        itemBuilder: ((context, index) {
+          Currency currency = currencies[index];
+          return Card(
+            elevation: 4,
+            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: ListTile(
+              leading: CircleAvatar(
+                  backgroundColor: Colors.amberAccent,
+                  child: Text(
+                    currency.name.substring(0, 3),
+                    style: const TextStyle(
+                      color: Colors.black,
+                    ),
+                  )),
+              title: Text(currency.name),
+              subtitle: Text.rich(TextSpan(children: [
+                TextSpan(text: 'Buy: ', children: [
+                  TextSpan(
+                      text: currency.buy.toStringAsFixed(2),
+                      style: const TextStyle(fontWeight: FontWeight.w600))
+                ]),
+                TextSpan(text: ' - Sell: ', children: [
+                  TextSpan(
+                      text: currency.sell.toStringAsFixed(2),
+                      style: const TextStyle(fontWeight: FontWeight.w600))
+                ]),
+                TextSpan(text: ' - Variation: ', children: [
+                  TextSpan(
+                      text: currency.variation.toStringAsFixed(2),
+                      style: const TextStyle(fontWeight: FontWeight.w600))
+                ]),
+              ])),
             ),
-          ));
-        }
-        if (snapshot.hasError) {
-          return Container(
-            color: Colors.red,
-            child: Center(
-                child: Text(
-              snapshot.error.toString(),
-              style: const TextStyle(
-                color: Colors.white12,
-                fontSize: 36,
-              ),
-            )),
           );
-        }
-
-        var currencies = snapshot.data;
-
-        return ListView.builder(
-            itemCount: currencies!.length,
-            itemBuilder: ((context, index) {
-              Currency currency = currencies[index];
-              return Card(
-                elevation: 4,
-                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                child: ListTile(
-                  leading: CircleAvatar(
-                      backgroundColor: Colors.amberAccent,
-                      child: Text(
-                        currency.name.substring(0, 3),
-                        style: const TextStyle(
-                          color: Colors.black,
-                        ),
-                      )),
-                  title: Text(currency.name),
-                  subtitle: Text.rich(TextSpan(children: [
-                    TextSpan(text: 'Buy: ', children: [
-                      TextSpan(
-                          text: currency.buy.toStringAsFixed(2),
-                          style: const TextStyle(fontWeight: FontWeight.w600))
-                    ]),
-                    TextSpan(text: ' - Sell: ', children: [
-                      TextSpan(
-                          text: currency.sell.toStringAsFixed(2),
-                          style: const TextStyle(fontWeight: FontWeight.w600))
-                    ]),
-                    TextSpan(text: ' - Variation: ', children: [
-                      TextSpan(
-                          text: currency.variation.toStringAsFixed(2),
-                          style: const TextStyle(fontWeight: FontWeight.w600))
-                    ]),
-                  ])),
-                ),
-              );
-            }));
-      },
-    );
+        }));
   }
 }
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -4,6 +4,7 @@ import 'package:f290_acf_dindin/service/currency_service.dart';
 import 'package:f290_acf_dindin/pages/converter/converter_page';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:loading_indicator/loading_indicator.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -23,11 +24,33 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('DinDin'),
       ),
-      body: IndexedStack(index: _selectedIndex, children: [
-        CurrenciesPage(),
-        StocksPage(),
-        QueryPage(),
-      ]),
+      body: FutureBuilder(
+        future: service.getCurrencies(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.none ||
+              snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+                child: SizedBox(
+              height: 200,
+              width: 200,
+              child: LoadingIndicator(
+                colors: [
+                  Colors.amberAccent,
+                ],
+                indicatorType: Indicator.lineSpinFadeLoader,
+              ),
+            ));
+          }
+
+          var currencies = snapshot.data;
+
+          return IndexedStack(index: _selectedIndex, children: [
+            CurrenciesPage(currencies: currencies!),
+            StocksPage(),
+            QueryPage(),
+          ]);
+        },
+      ),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.amberAccent,
         currentIndex: _selectedIndex,
